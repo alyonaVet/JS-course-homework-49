dbe2ec5b88402173c2667e5211f42e0055773962
alyonaVet <a.v.alenka2015@gmail.com>
2024-05-10 13:44:34 +0600

    refactor chessBoard: make it looks better

--- task-2/board.ts
+++ task-2/board.ts
@@ -1,12 +1,23 @@
+const alpha:string = "abcdefghijklmnopqrstuvwxyz";
 const chessBoard = (width: number, height: number): string => {
+    width = width < 27 ? width : 26
+    const letters:string = `  ${alpha.slice(0,width)}   \n`;
+    const horizBorder:string = `  ${'-'.repeat(width)}   \n`;
+
     let chessboard: string = '';
-    for (let i: number = 0; i < width; i++) {
-        for (let j: number = 0; j < height; j++) {
-            const cellColor: '#' | ' ' = (i + j) % 2 === 0 ? '#' : ' ';
+    chessboard += letters;
+    chessboard += horizBorder;
+
+    for (let i: number = 0; i < height; i++) {
+        chessboard += `| `;
+        for (let j: number = 0; j < width; j++) {
+            const cellColor: '\u25A0' | ' ' = (i + j) % 2 === 0 ? '\u25A0' : ' ';
             chessboard += cellColor;
         }
-        chessboard += '\n';
+        chessboard += ` | ${height-(i)}\n`;
     }
+    chessboard += horizBorder;
+
     return chessboard;
 };
 const chessboard: string = chessBoard(8, 8);
